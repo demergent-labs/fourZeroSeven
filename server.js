ce03aeb4423ca02562eaf5eb6d68cae3b93969ec
Benjamin DeMann <benjamin.demann@gmail.com>
2024-02-13 15:16:28 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Endpoint for 200 response
+app.get('/200', (req, res) => {
+  res.status(200).send('This is a 200 OK response.');
+});
+
+// Endpoint for 407 response
+app.get('/407', (req, res) => {
+  res.status(407).send('This is a 407 Proxy Authentication Required response.');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
